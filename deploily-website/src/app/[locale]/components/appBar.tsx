6863d67d7fa906604b8b481caefb5e9f84618050
Ali BENAHMED DAHO <ali.benahmeddaho@transformatek.dz>
2024-12-08 21:35:45 +0000

    rename app folder

--- deploily-website/src/app/[locale]/components/appBar.tsx
+++ deploily-website/src/app/[locale]/components/appBar.tsx
@@ -5,7 +5,6 @@ import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Button from '@mui/material/Button';
 import MenuIcon from '@mui/icons-material/Menu';
-import { useTranslations } from 'next-intl';
 import { Container, Grid, Link, Typography, alpha, } from '@mui/material';
 import LocaleSwitcher from '../../../components/locale/localeSwitcher';
 import { useTheme } from "@mui/material";
@@ -14,7 +13,6 @@ import { useEffect, useRef, useState } from 'react';
 
 function AppAppBar() {
 
-    const t = useTranslations("appBar");
     const [open, setOpen] = React.useState(false);
     const toggleDrawer = (newOpen: boolean) => () => {
         setOpen(newOpen);
@@ -59,7 +57,7 @@ function AppAppBar() {
     const appBarColor =
         theme.palette.mode == "dark" ? theme.palette.primary.main : theme.palette.secondary.main;
 
-    
+
     return (
         <>
             <AppBar
@@ -70,50 +68,50 @@ function AppAppBar() {
                     bgcolor: theme.palette.mode == "dark" ? alpha("#0c0d0f", 0.9) : alpha("#FFFFFF", 0.7),
                     backgroundImage: 'none',
                 }}
-            > 
-            <Toolbar>
+            >
+                <Toolbar>
                     <Container maxWidth={false} style={{ padding: "0px", maxWidth: "1280px" }} >
 
-                    <Grid container direction="row" spacing={8} sx={{ display: "flex", justifyContent: "space-between", }} alignItems="center" >
-                        <Grid item>
-                            <Grid container alignItems={"center"} marginLeft={"10px"} >
-                                <Image
-                                    src="/images/logo_name.png"
-                                    width={180}
-                                    height={70}
-                                    alt="logo-deploily"
-                                    style={{
-                                        marginRight: "20px"
-                                    }}
-                                />
-                              
+                        <Grid container direction="row" spacing={8} sx={{ display: "flex", justifyContent: "space-between", }} alignItems="center" >
+                            <Grid item>
+                                <Grid container alignItems={"center"} marginLeft={"10px"} >
+                                    <Image
+                                        src="/images/logo_name.png"
+                                        width={180}
+                                        height={70}
+                                        alt="logo-deploily"
+                                        style={{
+                                            marginRight: "20px"
+                                        }}
+                                    />
+
+                                </Grid>
                             </Grid>
-                        </Grid>
-                        <Grid item
-                            display={width < 905 ? "none" : 'block'}
-                            sx={{
-                                alignItems: "center",
-                            }}>
+                            <Grid item
+                                display={width < 905 ? "none" : 'block'}
+                                sx={{
+                                    alignItems: "center",
+                                }}>
                                 <Grid container spacing="20px" sx={{ alignItems: "center" }} display={{ sm: "flex", md: "flex", xs: "none" }}>
                                     <Grid item >
                                         <LocaleSwitcher color={appBarColor} />
                                     </Grid>
-                                </Grid> 
-                            <Grid display={{ sm: "none", md: "none", xs: "flex" }}>
-                                <LocaleSwitcher color={appBarColor} />
-                                <Button
-                                    variant="text"
-                                    color="primary"
-                                    aria-label="menu"
-                                    onClick={toggleDrawer(true)}
-                                    sx={{ minWidth: '30px', p: '0px' }}
-                                >
-                                    <MenuIcon sx={{ color: 'white' }} />
-                                </Button>
+                                </Grid>
+                                <Grid display={{ sm: "none", md: "none", xs: "flex" }}>
+                                    <LocaleSwitcher color={appBarColor} />
+                                    <Button
+                                        variant="text"
+                                        color="primary"
+                                        aria-label="menu"
+                                        onClick={toggleDrawer(true)}
+                                        sx={{ minWidth: '30px', p: '0px' }}
+                                    >
+                                        <MenuIcon sx={{ color: 'white' }} />
+                                    </Button>
 
+                                </Grid>
                             </Grid>
                         </Grid>
-                    </Grid>
                         {/* <ToggleColorMode color={appBarColor} /> */}
                     </Container>
                 </Toolbar>
