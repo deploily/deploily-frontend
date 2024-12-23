398348951f52a5bbfc4abc8e551ec3c58f3a93e9
Sedjai Bouchra <bouchra.sedjai@transformatek.dz>
2024-12-23 11:43:14 +0000

    Add footer.

---
+++ deploily-website/src/app/[locale]/components/utils/about.tsx
@@ -0,0 +1,20 @@
+export const socialMedias = [
+    {
+        lien: "https://www.facebook.com/deploily",
+        image: "/images/facebook.png",
+        alt: "Facebook",
+    }, {
+        lien: "https://www.linkedin.com/company/deploily",
+        image: "/images/linkedin.png",
+        alt: "LinkedIn",
+    }, {
+        lien: "https://transformatek.dz",
+        image: "/images/transformatek-icon.png",
+        alt: "Transformatek",
+    },
+    {
+        lien: "https://github.com/deploily",
+        image: "/images/github.png",
+        alt: "GitHub",
+    },
+]
