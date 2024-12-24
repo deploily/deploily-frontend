5d4c098a50916fb941d1b7ebc890a9e841b6b098
Sedjai Bouchra <bouchra.sedjai@transformatek.dz>
2024-12-24 14:11:36 +0000

    Add pre-commit tools.

--- deploily-website/src/components/locale/localeSwitcher.tsx
+++ deploily-website/src/components/locale/localeSwitcher.tsx
@@ -1,18 +1,18 @@
-import { FormControl, MenuItem, Select } from "@mui/material";
-import { useLocale } from "next-intl";
+import {FormControl, MenuItem, Select} from "@mui/material";
+import {useLocale} from "next-intl";
 
-import { useTransition } from "react";
-import { locales } from "../../config";
-import { usePathname, useRouter } from "../../navigation";
+import {useTransition} from "react";
+import {locales} from "../../config";
+import {usePathname, useRouter} from "../../navigation";
 
 export default function LocaleSwitcher() {
   const locale = useLocale();
   const router = useRouter();
   const [, startTransition] = useTransition();
   const pathname = usePathname();
-  const handleChange = (event: { target: { value: string } }) => {
+  const handleChange = (event: {target: {value: string}}) => {
     startTransition(() => {
-      router.replace(pathname, { locale: event.target.value });
+      router.replace(pathname, {locale: event.target.value});
     });
   };
   return (
@@ -32,7 +32,7 @@ export default function LocaleSwitcher() {
         onChange={handleChange}
       >
         {locales.map((loc) => (
-          <MenuItem key={loc} value={loc} sx={{ color: "white" }}>
+          <MenuItem key={loc} value={loc} sx={{color: "white"}}>
             {loc.toUpperCase()}
           </MenuItem>
         ))}
