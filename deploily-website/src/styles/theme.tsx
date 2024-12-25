a1cb7545cc224673df1cf188c5ee1a1bae0fef27
Sedjai Bouchra <bouchra.sedjai@transformatek.dz>
2024-12-25 15:36:59 +0000

    Add package antd & theming.

---
+++ deploily-website/src/styles/theme.tsx
@@ -0,0 +1,30 @@
+const primaryColor = "#206fba";
+const secondaryColor = "#eda879";
+
+export const theme = {
+  token: {
+    colorPrimary: primaryColor,
+    colorSecondary: secondaryColor,
+    colorInfo: "#206fba",
+    colorSuccess: "#2b7904",
+    colorWarning: "#fadb14",
+    colorError: "#f40e11",
+    colorLink: "#63a9ea",
+    colorTextBase: "#ccdae6",
+    colorBgBase: "#000000",
+    fontSize: 14,
+    colorDark: "#001529",
+    colorWhite: "#ffff",
+  },
+  components: {
+    Button: {
+      colorPrimary: primaryColor,
+      buttonBgHoverPrimary: "#333333",
+      algorithm: true,
+    },
+    Input: {
+      colorPrimary: "#eb2f96",
+      algorithm: true,
+    },
+  },
+};
