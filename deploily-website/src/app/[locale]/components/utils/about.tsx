07b01b97ffb6ea7428f04c8e339e3116cd5560cd
Sedjai Bouchra <bouchra.sedjai@transformatek.dz>
2024-12-25 09:05:25 +0000

    Fix icon docs.

--- deploily-website/src/app/[locale]/components/utils/about.tsx
+++ deploily-website/src/app/[locale]/components/utils/about.tsx
@@ -1,24 +1,24 @@
 export const socialMedias = [
-    {
-        lien: "https://www.facebook.com/deploily",
-        image: "/images/facebook.png",
-        alt: "Facebook",
-    }, {
-        lien: "https://www.linkedin.com/showcase/deploily",
-        image: "/images/linkedin.png",
-        alt: "LinkedIn",
-    }, {
-        lien: "https://transformatek.dz",
-        image: "/images/transformatek-icon.png",
-        alt: "Transformatek",
-    },
-    {
-        lien: "https://github.com/deploily",
-        image: "/images/github.png",
-        alt: "GitHub",
-    },{
-        lien: "http://docs.deploily.cloud/",
-        image: "/images/deploily_logo.png",
-        alt: "Deploily Docs",
-    },
+  {
+    lien: "https://www.facebook.com/deploily",
+    image: "/images/facebook.png",
+    alt: "Facebook",
+  }, {
+    lien: "https://www.linkedin.com/showcase/deploily",
+    image: "/images/linkedin.png",
+    alt: "LinkedIn",
+  }, {
+    lien: "https://transformatek.dz",
+    image: "/images/transformatek-icon.png",
+    alt: "Transformatek",
+  },
+  {
+    lien: "https://github.com/deploily",
+    image: "/images/github.png",
+    alt: "GitHub",
+  }, {
+    lien: "http://docs.deploily.cloud/",
+    image: "/images/book.png",
+    alt: "Deploily Docs",
+  },
 ]
